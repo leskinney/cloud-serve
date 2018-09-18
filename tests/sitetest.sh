33ac0976d339fecc416db0ec75107919923852ca
Les Kinney <les.kinney@aa.com>
2018-09-18 11:40:23 -0700

    New Build

--- tests/sitetest.sh
+++ tests/sitetest.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-if curl http://serve:5000 | grep display/site/; then
-  echo "Site exists.\nSite test passes.\n"
+if curl http://serve:5000 | grep site/; then
+  echo "Site 'site/' exists.\nSite test passes.\n"
   exit 0
 else
-  echo "Site does not exist.\nSite test fails"
+  echo "Site 'site/' does not exist.\nSite test fails"
   exit 1
 fi
